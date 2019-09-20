4c11ab2a45432d2157b29a4a90a1bb452bedb59e
Mark Stone <markwstone85@gmail.com>
2019-09-20 20:45:59 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(integers)
+  integers.sort do |a, b|
+    a <=> b 
+  end
+end
 
+def sort_array_desc(integers)
+  integers.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+  end
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  array.each do |i|
+    i[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select{|i| i.start_with?("a")} 
+end
+
+def sum_array(array)
+  array.sum
+end 
+
+def add_s(array)
+  array.collect.each.with_index do |element, index|
+    if index != 1 
+      element + "s"
+    else
+      element 
+    end 
+  end 
+end 
